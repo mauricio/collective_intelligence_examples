c467e12c26ee1c496ced78c3f17987f8bb014e69
Maurício <mauricio.linhares@gmail.com>
2008-09-19 00:39:49 -0300

    Updating the users ratings query

--- vendor/plugins/acts_as_recommendable/lib/rating.rb
+++ vendor/plugins/acts_as_recommendable/lib/rating.rb
@@ -15,13 +15,8 @@ class Rating < ActiveRecord::Base
   class << self
 
     def find_common_ratings_for_users( first_user, last_user, rated_type = 'Movie' )
-      rows = connection.select_all( sanitize_sql( [ COMMON_RATINGS_FOR_USERS_SELECT, first_user.id, last_user.id, rated_type ] ) )
-      return nil, nil if rows.blank?
-      first_user_ratings, last_user_ratings = [], []
-      rows.each do |row|
-        first_user_ratings << Rating.new( :user_id => first_user.id, :score => row['left_score'], :rated_id => row['rated_id'], :rated_type => row['rated_type'] )
-        last_user_ratings << Rating.new( :user_id => last_user.id, :score => row['right_score'], :rated_id => row['rated_id'], :rated_type => row['rated_type'] )
-      end
+      first_user_ratings = find_by_sql( [ COMMON_RATINGS_FOR_USERS_SELECT, first_user.id, rated_type, last_user.id, rated_type ] )
+      last_user_ratings = find( :all, :order => 'rated_id', :conditions => ['user_id = ? and rated_type = ? and rated_id in ( ? )', last_user.id, rated_type, first_user_ratings.map(&:rated_id)] )
       return first_user_ratings, last_user_ratings
     end
 
@@ -37,14 +32,11 @@ class Rating < ActiveRecord::Base
     FROM ratings r 
     WHERE r.rated_id = ? AND r.rated_type = ? AND r.user_id IN 
     ( SELECT ra.user_id FROM ratings ra WHERE ra.rated_id = ? AND ra.rated_type = ? )
-    ORDER BY r.user_id
- }
+    ORDER BY r.user_id }
   
-  COMMON_RATINGS_FOR_USERS_SELECT = %q{
- SELECT left_r.rated_id, left_r.rated_type, left_r.user_id as 'left_user', left_r.score as 'left_score', right_r.score as 'right_score', right_r.user_id as 'right_user'
- FROM ratings left_r
- INNER JOIN ratings right_r
- ON left_r.user_id != right_r.user_id AND left_r.rated_id = right_r.rated_id AND left_r.rated_type = right_r.rated_type
- WHERE left_r.user_id = ? and right_r.user_id = ? order by left_r.user_id and left_r.rated_id}
+  COMMON_RATINGS_FOR_USERS_SELECT = %q{SELECT r.* 
+  FROM ratings r WHERE r.user_id = ? AND r.rated_type = ? AND r.rated_id IN 
+  ( SELECT ra.rated_id FROM ratings ra WHERE ra.user_id = ? and ra.rated_type = ? )
+  ORDER BY r.rated_id}
 
 end
